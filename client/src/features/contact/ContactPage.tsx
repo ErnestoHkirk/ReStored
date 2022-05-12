e171a51ad6d1df653ea4ee78175cb14faf6307dd
ErnestoHKirk <hooghkirk.e@gmail.com>
2022-05-11 23:12:24 -0700

    Add react-redux

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,9 +1,15 @@
 import { Typography } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+
 
 export default function ContactPage() {
-    return (
-        <Typography variant='h2'>
-            Contact page
-        </Typography>
-    )
+  const dispatch = useAppDispatch();
+  const { data, title } = useAppSelector(state => state.counter)
+
+  return (
+    <>
+      <Typography variant="h2">{title}</Typography>
+      <Typography variant="h5">The data is: {data}</Typography>
+    </>
+  );
 }
